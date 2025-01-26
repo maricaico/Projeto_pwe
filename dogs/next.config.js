a2e1b05aa8293e2c07df8804429b90b28220f52e
Mariana <mari_caico@yahoo.com.br>
2025-01-26 16:26:40 -0300

    Inclusão next.config.js

---
+++ dogs/next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+      remotePatterns: [
+          {
+              protocol: "https",
+              hostname: "cdn2.thedogapi.com",
+              pathname: "/images/**",
+          },
+      ],
+  },
+};
+
+module.exports = nextConfig;
+
+
